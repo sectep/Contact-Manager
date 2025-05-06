4e307300a22678d040339d8f7ac007c03bd26a40
sectepOfficial <faraonmakster@gmail.com>
2025-05-06 20:03:17 +0300

    Add files via upload
    Uploaded new features.

--- ContactEntrence.java
+++ ContactEntrence.java
@@ -4,11 +4,24 @@ import java.util.Scanner;
 
 class ContactEntrence {
     private int option, phoneNumber, categoryOption;
-    private String keepUsing, email, firstName, secondName;
+    private String email, firstName, secondName;
     private Category category;
 
     private Scanner console = new Scanner(System.in);
 
+    public void startApp() {
+        display();
+
+        option = askOption();
+        if (option != -1) {
+            chooseOption(option);
+            return;
+        }
+
+        System.out.println("Invalid type!Write an integer value(1-5)");
+    }
+
+    // method, which display the menu.
     private void display() {
         System.out.println("=== CONTACT MANAGER ===");
         System.out.println("1. Add contact");
@@ -18,31 +31,18 @@ class ContactEntrence {
         System.out.println("5. Exit");
     }
 
-    public void startApp() {
-        display();
+    // method which reads the chosen option from user.
+    private int askOption() {
         System.out.print("Choose: ");
-
-        // keep asking the user if he writes other type, then int.
-        while (true) {
-            if (console.hasNextInt()) {
-                option = console.nextInt();
-                console.nextLine();
-                break;
-            } else {
-                System.out.println("Invalide option type!\n");
-                System.out.print("Do you wish to continue?(Y/N)");
-                keepUsing = console.nextLine();
-                if (!(keepUsing.toUpperCase().equals("Y"))) {
-                    System.out.println("Leaving the interface");
-                    break;
-                }
-            }
+        if (console.hasNextInt()) {
+            option = console.nextInt();
+            console.nextLine();
+            return option;
         }
-
-        chooseOption(option);
+        return -1; // if it is invalid type, return -1;
     }
 
-    // start the function of ContactManager, based on the option.
+    // method, which starts the function of ContactManager, based on the option.
     private void chooseOption(int what) {
         switch (what) {
             case 1:
@@ -57,6 +57,13 @@ class ContactEntrence {
                 email = askEmail();
                 ContactManager.readContact(email);
                 break;
+            case 3:
+                firstName = askFirstName();
+                ContactManager.removeContact(firstName);
+                break;
+            case 4:
+                ContactManager.showAll();
+                break;
             case 5: {
                 System.out.println("Leaving the program...");
                 break;
@@ -67,28 +74,28 @@ class ContactEntrence {
         }
     }
 
-    // ask first name from the user.
+    // method, which asks for a first name from the user.
     private String askFirstName() {
         System.out.print("Write first name: ");
         firstName = console.nextLine();
         return firstName;
     }
 
-    // ask second name from the user.
+    // method, which asks for a second name from the user.
     private String askSecondName() {
         System.out.print("Write second name: ");
         secondName = console.nextLine();
         return secondName;
     }
 
-    // ask email from the user.
+    // method, which asks for an email from the user.
     private String askEmail() {
         System.out.print("Write email: ");
         email = console.nextLine();
         return email;
     }
 
-    // ask phone number from the user.
+    // method, which asks for a phone number from the user.
     private int askPhoneNumber() {
         System.out.print("Write phone number: ");
         if (console.hasNextInt()) {
@@ -98,7 +105,7 @@ class ContactEntrence {
         return phoneNumber;
     }
 
-    // ask the category of contact from the user.
+    // method, which asks the category of contact from the user.
     private Category askCategory() {
         displayCategory();
 
@@ -111,7 +118,7 @@ class ContactEntrence {
         return useCategory(categoryOption);
     }
 
-    // display category options.
+    // method, which displays category options.
     private void displayCategory() {
         System.out.println("\n=== CATEGORY ===");
         System.out.println("1. FRIEND");
@@ -120,7 +127,7 @@ class ContactEntrence {
         System.out.println("Any other number for OTHER");
     }
 
-    // return the category, based on answer
+    // method, which returns the category, based on the chosen number from the bar.
     private Category useCategory(int num) {
         switch (categoryOption) {
             case 1:
@@ -134,5 +141,4 @@ class ContactEntrence {
         }
         return Category.OTHER;
     }
-
 }
--- ContactManager.java
+++ ContactManager.java
@@ -1,11 +1,15 @@
 package contactmanager;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactManager {
     static String line, currentEmail, currentName, currentSecondName, currentNum, currentCategory;
+    static List<String> lines = new ArrayList<>();
+    static List<String> list = new ArrayList<>();
 
-    // a method, which adds new contact to contact.txt
+    // method, which adds new contact to contact.txt
     static void addContact(String firstName, String secondName, int num, String email, Category category) {
         try (BufferedWriter out = new BufferedWriter(new FileWriter("contactmanager\\contact.txt", true))) {
 
@@ -25,7 +29,7 @@ public class ContactManager {
         }
     }
 
-    // read contatcs by email.
+    // method, which reads contatcs by email.
     static void readContact(String email) {
         try (BufferedReader in = new BufferedReader(new FileReader("contactmanager\\contact.txt"))) {
 
@@ -65,10 +69,9 @@ public class ContactManager {
             System.out.println("Exception has occurred while method" +
                     "was checking for email existanse");
         }
-
     }
 
-    // check if the email exists.
+    // method, which checks if the email exists.
     private static boolean emailExists(String email) {
         try (BufferedReader in = new BufferedReader(new FileReader("contactmanager\\contact.txt"))) {
 
@@ -89,4 +92,93 @@ public class ContactManager {
         return false;
     }
 
+    // method, which removes the contact from the database.
+    static void removeContact(String contact) {
+
+        if (contactExists(contact)) {
+            removeData(contact);
+            writeToFile();
+            System.out.println(contact + " has been removed successfully.");
+            return;
+        }
+    }
+
+    // method, which checks if the current contact exists.
+    private static boolean contactExists(String contact) {
+        try (BufferedReader in = new BufferedReader(new FileReader("contactmanager\\contact.txt"))) {
+
+            while ((line = in.readLine()) != null) {
+                if (line.startsWith("@")) {
+                    currentName = line.substring(1);
+
+                    if (contact.equals(currentName)) {
+                        return true;
+                    }
+                }
+            }
+        } catch (IOException exc) {
+            System.out.println("Exception has occurred while method" +
+                    "was checking for email existanse");
+        }
+
+        return false;
+
+    }
+
+    // method, which removes the information.
+    private static void removeData(String contact) {
+        for (int i = 0; i < lines.size(); i++) {
+            // check if the line contains the contact name in the first line of the block.
+            if (lines.get(i).startsWith("@") && lines.get(i).substring(1).equals(contact)) {
+                for (int j = 0; j < 6; j++) {
+                    lines.remove(i);
+                }
+                return;
+            }
+        }
+    }
+
+    // a method, which writes to the file.
+    private static void writeToFile() {
+        try (BufferedWriter out = new BufferedWriter(new FileWriter("contactmanager\\contact.txt"))) {
+            for (int i = 0; i < lines.size(); i++) {
+                out.write(lines.get(i));
+                out.newLine();
+            }
+            return;
+        } catch (IOException exc) {
+            System.out.println("Exception has occurred, while method was rewriting the file.");
+            ;
+        }
+    }
+
+    // method, which showes all contacts from the database.
+    static void showAll() {
+        dataToArray();
+        if (lines != null) {
+            showArrayData();
+            return;
+        }
+        System.out.println("Your list is empty.");
+    }
+
+    // method, which writes contact.txt data to array.
+    private static void dataToArray() {
+        try (BufferedReader in = new BufferedReader(new FileReader("contactmanager\\contact.txt"))) {
+
+            while ((line = in.readLine()) != null) {
+                list.add(line);
+            }
+        } catch (IOException exc) {
+            System.out.println("Exception has occured , while reading the contact");
+        }
+    }
+
+    // show the information of array.
+    private static void showArrayData() {
+        System.out.println("===CONTACTS===");
+        for (String line : list) {
+            System.out.println(line.substring(1));
+        }
+    }
 }
